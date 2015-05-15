599dbc7e824faea658384aa7ea6d0ca585a8a9b8
Abhijeet Mohan <void.aby@gmail.com>
2015-05-15 20:37:14 +0530

    Update notifications-impl.js

--- adapters/notifications/notifications-impl.js
+++ adapters/notifications/notifications-impl.js
@@ -1,9 +1,3 @@
-/*
- *  Licensed Materials - Property of IBM
- *  5725-G92 (C) Copyright IBM Corp. 2011, 2013. All Rights Reserved.
- *  US Government Users Restricted Rights - Use, duplication or
- *  disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
- */
 
 /************************************************************************
  * Implementation code for procedure - 'procedure1'
