57e4f6760b68dbd3f488ae77df8b71501058f9ac
Abhijeet Mohan <void.aby@gmail.com>
2016-02-16 22:26:40 +0530

    fixed styling

--- apps/mfpizza/common/js/maps.js
+++ apps/mfpizza/common/js/maps.js
@@ -1,102 +1,96 @@
 /**
  * 	For showing directions on google maps into the application
  */
+var map,
+    currentPosition,
+    directionsDisplay,
+    directionsService,
+
+    // lat and lng location of destination
+    destinationLatitude,
+    destinationLongitude,
+    map_canvas, // Id of the element where map will be shown
+    info_message, // Message on the current marker
+    direction_panel, // Id of the Panel element where final directions will be shown
+    panel_container; //Id of panel container
+
+// Initialize map	
+function mapInit(desLat, desLong, directionPanel, panelContainer, mapCanvas, message) {
+    directionsDisplay = new google.maps.DirectionsRenderer();
+    directionsService = new google.maps.DirectionsService();
+    destinationLatitude = desLat;
+    destinationLongitude = desLong;
+    direction_panel = directionPanel;
+    map_canvas = mapCanvas;
+    info_message = message;
+    panel_container = "#" + panelContainer;
+}
+
+function initializeMapAndCalculateRoute(lat, lon) {
+
+    currentPosition = new google.maps.LatLng(lat, lon);
+
+    map = new google.maps.Map(document.getElementById(map_canvas), {
+        zoom: 15,
+        center: currentPosition,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+
+    directionsDisplay.setMap(map);
+
+    var currentPositionMarker = new google.maps.Marker({
+        position: currentPosition,
+        map: map,
+        title: "Current position"
+    });
+
+    // current position marker info
+
+    var infowindow = new google.maps.InfoWindow();
+    google.maps.event.addListener(currentPositionMarker, 'click', function() {
+        infowindow.setContent(info_message);
+        infowindow.open(map, currentPositionMarker);
+    });
 
 
-     var map,
-                currentPosition,
-                directionsDisplay,
-                directionsService,
-                
-                // lat and lng location of destination
-                destinationLatitude,
-                destinationLongitude,
-				map_canvas, // Id of the element where map will be shown
-				info_message, // Message on the current marker
-				direction_panel, // Id of the Panel element where final directions will be shown
-				panel_container; //Id of panel container
-				
-			// Initialize map	
-			function mapInit(desLat,desLong,directionPanel,panelContainer,mapCanvas,message)
-			{
-				directionsDisplay = new google.maps.DirectionsRenderer();
-                directionsService = new google.maps.DirectionsService();
-				destinationLatitude = desLat;
-                destinationLongitude = desLong;
-				direction_panel=directionPanel;
-				map_canvas=mapCanvas;
-				info_message=message;
-				panel_container = "#"+panelContainer;
-			}
 
-            function initializeMapAndCalculateRoute(lat, lon)
-            {
+    calculateRoute();
+}
 
-                currentPosition = new google.maps.LatLng(lat, lon);
+function locError(error) {
+    // the current position could not be located
+    alert("Some Error Occurred Connecting Google Maps");
+}
 
-                map = new google.maps.Map(document.getElementById(map_canvas), {
-                   zoom: 15,
-                   center: currentPosition,
-                   mapTypeId: google.maps.MapTypeId.ROADMAP
-                 });
+function locSuccess(position) {
+    // initialize map with current position and calculate the route
+    initializeMapAndCalculateRoute(position.coords.latitude, position.coords.longitude);
+}
 
-                directionsDisplay.setMap(map);
+// calculates route b/w given target n destination
+function calculateRoute() {
 
-                 var currentPositionMarker = new google.maps.Marker({
-                    position: currentPosition,
-                    map: map,
-                    title: "Current position"
-                });
+    var targetDestination = new google.maps.LatLng(destinationLatitude, destinationLongitude);
+    if (currentPosition != '' && targetDestination != '') {
 
-                // current position marker info
-                
-                var infowindow = new google.maps.InfoWindow();
-                google.maps.event.addListener(currentPositionMarker, 'click', function() {
-                    infowindow.setContent(info_message);
-                    infowindow.open(map, currentPositionMarker);
-                });
-                
+        var request = {
+            origin: currentPosition,
+            destination: targetDestination,
+            travelMode: google.maps.DirectionsTravelMode["DRIVING"]
+        };
 
-                
-                calculateRoute();
+        directionsService.route(request, function(response, status) {
+            if (status == google.maps.DirectionsStatus.OK) {
+                directionsDisplay.setPanel(document.getElementById(direction_panel));
+                directionsDisplay.setDirections(response);
+
+                console.log(response);
+                $(panel_container).show();
+            } else {
+                $(panel_container).hide();
             }
-
-            function locError(error) {
-               // the current position could not be located
-			   alert("Some Error Occurred Connecting Google Maps");
-            }
-
-            function locSuccess(position) {
-                // initialize map with current position and calculate the route
-                initializeMapAndCalculateRoute(position.coords.latitude, position.coords.longitude);
-            }
-
-		// calculates route b/w given target n destination
-            function calculateRoute() {
-
-                var targetDestination =  new google.maps.LatLng(destinationLatitude, destinationLongitude);
-                if (currentPosition != '' && targetDestination != '') {
-
-                    var request = {
-                        origin: currentPosition,
-                        destination: targetDestination,
-                        travelMode: google.maps.DirectionsTravelMode["DRIVING"]
-                    };
-
-                    directionsService.route(request, function(response, status) {
-                        if (status == google.maps.DirectionsStatus.OK) {
-                            directionsDisplay.setPanel(document.getElementById(direction_panel));
-                            directionsDisplay.setDirections(response);
-							
-                            console.log(response);
-                            $(panel_container).show();
-                        }
-                        else {
-                            $(panel_container).hide();
-                        }
-                    });
-                }
-                else {
-                    $(panel_container).hide();
-                }
-            }
+        });
+    } else {
+        $(panel_container).hide();
+    }
+}
